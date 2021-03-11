90d72db9657e99680f6575dcd885a9787c8e7b77
yuan_wy_wang <631824375@qq.com>
2021-03-11 19:14:31 +0800

    剩余提交

--- src/config/test.js
+++ src/config/test.js
@@ -1,6 +1,6 @@
 import { setValue, getValue } from './RedisConfig'
 
-// setValue('address', '广东省江门市江海区')
+setValue('kkkkk', '广东省江门市江海区')
 
 getValue('address').then(res => {
   console.log(res, 'result')
