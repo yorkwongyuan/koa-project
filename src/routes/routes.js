e60e6250e1ae18c2dbef4fa051131cd5f091f5e7
yuan_wy_wang <631824375@qq.com>
2021-06-05 18:54:20 +0800

    路由配置

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 import combineRouter from 'koa-combine-routers'
-import requireContext from 'require-context'
 import path from 'path'
-const modules = requireContext(path.resolve(__dirname, './modules'), true, /\.js$/)
+const requireContext = require('require-context')
+const modules = requireContext(path.resolve(__dirname, './files'), true, /\.js$/)
 const files = modules.keys().reduce((item, path) => {
   const value = modules(path)
   item.push(value.default)
